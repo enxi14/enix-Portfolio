d21e13bfbe7bc9d9ad2eb5b0cd92004007f09f3e
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-13 17:36:44 -0400

    commit

--- Script.js
+++ Script.js
@@ -1,19 +1,19 @@
-// Get the dark mode toggle button and the <body> element
+// Get the dark mode toggle button, the <body> element, and the #page-content element
 const darkModeToggle = document.getElementById("dark-mode-toggle");
-const body = document.body;
-
-// Check if the user has a preference for dark mode
-const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+const body = document.getElementById("body");
+const pageContent = document.getElementById("page-content");
 
 // Function to toggle between light and dark mode
 function toggleDarkMode() {
     if (body.classList.contains("dark-mode")) {
         // If currently in dark mode, switch to light mode
         body.classList.remove("dark-mode");
+        pageContent.classList.remove("dark-mode");
         darkModeToggle.textContent = "Dark Mode";
     } else {
         // If currently in light mode, switch to dark mode
         body.classList.add("dark-mode");
+        pageContent.classList.add("dark-mode");
         darkModeToggle.textContent = "Light Mode";
     }
 }
@@ -22,7 +22,9 @@ function toggleDarkMode() {
 darkModeToggle.addEventListener("click", toggleDarkMode);
 
 // Initialize dark mode based on user preference
+const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 if (prefersDarkMode.matches) {
     body.classList.add("dark-mode");
+    pageContent.classList.add("dark-mode");
     darkModeToggle.textContent = "Light Mode";
 }
