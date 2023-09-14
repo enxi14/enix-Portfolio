1b376ebc25e1161cf433844cff62d92c5ad56528
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-14 19:05:46 -0400

    commit

--- Script.js
+++ Script.js
@@ -1,29 +1,21 @@
 
 const darkModeToggle = document.getElementById("dark-mode-toggle");
 const body = document.getElementById("body");
-const pageContent = document.getElementById("page-content");
 
-
+function toggleDarkMode() {
     if (body.classList.contains("dark-mode")) {
-        
         body.classList.remove("dark-mode");
-        pageContent.classList.remove("dark-mode");
         darkModeToggle.textContent = "Dark Mode";
     } else {
-        
         body.classList.add("dark-mode");
-        pageContent.classList.add("dark-mode");
         darkModeToggle.textContent = "Light Mode";
     }
-
-
+}
 
 darkModeToggle.addEventListener("click", toggleDarkMode);
-
 
 const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 if (prefersDarkMode.matches) {
     body.classList.add("dark-mode");
-    pageContent.classList.add("dark-mode");
     darkModeToggle.textContent = "Light Mode";
 }
