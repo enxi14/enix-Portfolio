b59a13f4a95faee31f806238a28581e61c84b789
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-16 15:26:14 -0400

    commit

--- project-Enix/javascript/Script.js
+++ project-Enix/javascript/Script.js
@@ -1,82 +1,91 @@
+const slider = document.querySelector('.slider');
+let imageIndex = 0;
+const images = document.querySelectorAll('.slider img');
 
-const darkModeToggle = document.getElementById("dark-mode-toggle");
-const body = document.body;
+function nextSlide() {
+    imageIndex = (imageIndex + 1) % images.length;
+    updateSlider();
+}
 
-function toggleDarkMode() {
-    if (body.classList.contains("dark-mode")) {
-        body.classList.remove("dark-mode");
-        darkModeToggle.textContent = "Dark Mode";
+function previousSlide() {
+    imageIndex = (imageIndex - 1 + images.length) % images.length;
+    updateSlider();
+}
+
+function updateSlider() {
+    const translateX = -imageIndex * 100;
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+
+setInterval(nextSlide, 3000); 
+
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+if (prevButton && nextButton) {
+    prevButton.addEventListener('click', previousSlide);
+    nextButton.addEventListener('click', nextSlide);
+}
+
+const slider = document.querySelector('.slider');
+let imageIndex = 0;
+const images = document.querySelectorAll('.slider img');
+
+function nextSlide() {
+    imageIndex = (imageIndex + 1) % images.length;
+    updateSlider();
+}
+
+function previousSlide() {
+    imageIndex = (imageIndex - 1 + images.length) % images.length;
+    updateSlider();
+}
+
+function updateSlider() {
+    const translateX = -imageIndex * 100;
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+setInterval(nextSlide, 3000); 
+
+
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+if (prevButton && nextButton) {
+    prevButton.addEventListener('click', previousSlide);
+    nextButton.addEventListener('click', nextSlide);
+}
+
+// Get the dark mode toggle button and store it in a variable
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+
+// Check if dark mode preference is stored in localStorage
+const isDarkMode = localStorage.getItem('darkMode') === 'enabled';
+
+// Function to enable dark mode
+function enableDarkMode() {
+    document.body.classList.add('dark-mode');
+    localStorage.setItem('darkMode', 'enabled');
+}
+
+// Function to disable dark mode
+function disableDarkMode() {
+    document.body.classList.remove('dark-mode');
+    localStorage.setItem('darkMode', 'disabled');
+}
+
+// Toggle dark mode when the button is clicked
+darkModeToggle.addEventListener('click', () => {
+    if (document.body.classList.contains('dark-mode')) {
+        disableDarkMode();
     } else {
-        body.classList.add("dark-mode");
-        darkModeToggle.textContent = "Light Mode";
+        enableDarkMode();
     }
-}
+});
 
-darkModeToggle.addEventListener("click", toggleDarkMode);
+// Set initial dark mode state based on user preference
+if (isDarkMode) {
+    enableDarkMode();
 
-
-const prefersDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
-if (prefersDarkMode.matches) {
-    body.classList.add("dark-mode");
-    darkModeToggle.textContent = "Light Mode";
-}
-
-const slider = document.querySelector('.slider');
-let imageIndex = 0;
-const images = document.querySelectorAll('.slider img');
-
-function nextSlide() {
-    imageIndex = (imageIndex + 1) % images.length;
-    updateSlider();
-}
-
-function previousSlide() {
-    imageIndex = (imageIndex - 1 + images.length) % images.length;
-    updateSlider();
-}
-
-function updateSlider() {
-    const translateX = -imageIndex * 100;
-    slider.style.transform = `translateX(${translateX}%)`;
-}
-
-
-setInterval(nextSlide, 3000); 
-
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-
-if (prevButton && nextButton) {
-    prevButton.addEventListener('click', previousSlide);
-    nextButton.addEventListener('click', nextSlide);
-}
-
-const slider = document.querySelector('.slider');
-let imageIndex = 0;
-const images = document.querySelectorAll('.slider img');
-
-function nextSlide() {
-    imageIndex = (imageIndex + 1) % images.length;
-    updateSlider();
-}
-
-function previousSlide() {
-    imageIndex = (imageIndex - 1 + images.length) % images.length;
-    updateSlider();
-}
-
-function updateSlider() {
-    const translateX = -imageIndex * 100;
-    slider.style.transform = `translateX(${translateX}%)`;
-}
-
-setInterval(nextSlide, 3000); 
-
-
-const prevButton = document.querySelector('.prev-button');
-const nextButton = document.querySelector('.next-button');
-
-if (prevButton && nextButton) {
-    prevButton.addEventListener('click', previousSlide);
-    nextButton.addEventListener('click', nextSlide);
-}
