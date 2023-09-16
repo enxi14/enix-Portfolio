708c63ec9bb995e0df7baf38d14559b1d33a6d97
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-16 09:36:14 -0400

    commit

--- project-Enix/javascript/Script.js
+++ project-Enix/javascript/Script.js
@@ -20,3 +20,34 @@ if (prefersDarkMode.matches) {
     body.classList.add("dark-mode");
     darkModeToggle.textContent = "Light Mode";
 }
+
+const slider = document.querySelector('.slider');
+let imageIndex = 0;
+const images = document.querySelectorAll('.slider img');
+
+function nextSlide() {
+    imageIndex = (imageIndex + 1) % images.length;
+    updateSlider();
+}
+
+function previousSlide() {
+    imageIndex = (imageIndex - 1 + images.length) % images.length;
+    updateSlider();
+}
+
+function updateSlider() {
+    const translateX = -imageIndex * 100;
+    slider.style.transform = `translateX(${translateX}%)`;
+}
+
+setInterval(nextSlide, 3000); // Auto slide every 3 seconds
+
+// Optionally, you can add buttons or other controls to manually navigate the slider.
+// Here's an example of adding previous and next buttons:
+const prevButton = document.querySelector('.prev-button');
+const nextButton = document.querySelector('.next-button');
+
+if (prevButton && nextButton) {
+    prevButton.addEventListener('click', previousSlide);
+    nextButton.addEventListener('click', nextSlide);
+}
