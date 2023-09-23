4e58c88ed35b37aa6c3bcc418fe3db512099a6b5
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-23 13:03:00 -0400

    commit

--- project-Enix/javascript/Script.js
+++ project-Enix/javascript/Script.js
@@ -13,3 +13,39 @@ function toggleMode() {
 }
 
 modeToggle.addEventListener('click', toggleMode);
+
+document.addEventListener("DOMContentLoaded", function () {
+    const fadeElements = document.querySelectorAll(".fade");
+    const slideElements = document.querySelectorAll(".slide");
+
+    function checkFadeSlide() {
+        fadeElements.forEach(function (element) {
+            if (isElementInViewport(element)) {
+                element.style.opacity = 1;
+            }
+        });
+
+        slideElements.forEach(function (element) {
+            if (isElementInViewport(element)) {
+                element.style.transform = "translateY(0)";
+            }
+        });
+    }
+
+    function isElementInViewport(el) {
+        const rect = el.getBoundingClientRect();
+        return (
+            rect.top >= 0 &&
+            rect.left >= 0 &&
+            rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+            rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+        );
+    }
+
+    // Initial check when the page loads
+    checkFadeSlide();
+
+    // Check on scroll
+    window.addEventListener("scroll", checkFadeSlide);
+});
+
