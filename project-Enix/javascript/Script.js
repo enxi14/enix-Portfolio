a679064dd4f9514152f3a1fdafb0282c98b885a3
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-26 17:33:40 -0400

    commit

--- project-Enix/javascript/Script.js
+++ project-Enix/javascript/Script.js
@@ -39,12 +39,12 @@ const modeToggle = document.getElementById('mode-toggle');
     const darkModeToggle = document.getElementById("darkModeToggle");
     const body = document.body;
 
-  darkModeToggle.addEventListener("change", () => {
+darkModeToggle.addEventListener("change", () => {
     if (darkModeToggle.checked) {
         body.classList.add("dark-mode");
     } else {
         body.classList.remove("dark-mode");
     }
-  });
+});
 
 
