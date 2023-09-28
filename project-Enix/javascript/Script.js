da3b80c8037472bcf9672ff2fd60867310b0556d
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-09-27 23:57:22 -0400

    commit

--- project-Enix/javascript/Script.js
+++ project-Enix/javascript/Script.js
@@ -37,39 +37,12 @@ const modeToggle = document.getElementById('mode-toggle');
 
 
 
-const darkModeToggle = document.getElementById('darkModeToggle');
-const modeIcon = document.getElementById('modeIcon');
-const body = document.body;
-
-darkModeToggle.addEventListener('change', () => {
-    if (darkModeToggle.checked) {
-        body.classList.add('dark-mode');
-        modeIcon.classList.add('fa-moon'); // If you're using Font Awesome, add moon icon class
-        modeIcon.classList.remove('fa-sun'); // Remove sun icon class
-    } else {
-        body.classList.remove('dark-mode');
-        modeIcon.classList.add('fa-sun'); // Add sun icon class
-        modeIcon.classList.remove('fa-moon'); // Remove moon icon class
-    }
-});
-
-// Initialize mode based on user's preference (you can use local storage for this)
-if (localStorage.getItem('darkMode') === 'enabled') {
-    darkModeToggle.checked = true;
-    body.classList.add('dark-mode');
-    modeIcon.classList.add('fa-moon'); // If you're using Font Awesome, add moon icon class
-    modeIcon.classList.remove('fa-sun'); // Remove sun icon class
-} else {
-    body.classList.remove('dark-mode');
-    modeIcon.classList.add('fa-sun'); // Add sun icon class
-    modeIcon.classList.remove('fa-moon'); // Remove moon icon class
-}
-
-// Save user's preference in local storage
-darkModeToggle.addEventListener('change', () => {
-    if (darkModeToggle.checked) {
-        localStorage.setItem('darkMode', 'enabled');
-    } else {
-        localStorage.setItem('darkMode', 'disabled');
-    }
+$(document).ready(function () {
+    $("#darkModeToggle").change(function () {
+        if (this.checked) {
+            $("body").addClass("dark-mode");
+        } else {
+            $("body").removeClass("dark-mode");
+        }
+    });
 });
