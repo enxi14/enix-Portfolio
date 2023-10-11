eff804d47c6efe3743f1e24e5f6e080c4e99dbe9
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-10 21:35:46 -0400

    commit

--- project-Enix/javascript/quiz.js
+++ project-Enix/javascript/quiz.js
@@ -10,46 +10,7 @@ $(document).ready(function () {
             options: ["Mars", "Venus", "Jupiter", "Mercury"],
             correctAnswer: "Mars"
         },
-        {
-            question: "What is the largest mammal in the world?",
-            options: ["Elephant", "Giraffe", "Blue Whale", "Hippopotamus"],
-            correctAnswer: "Blue Whale"
-        },
-        {
-            question: "What is the chemical symbol for gold?",
-            options: ["Go", "Gd", "Au", "Ag"],
-            correctAnswer: "Au"
-        },
-        {
-            question: "Which gas do plants absorb from the atmosphere?",
-            options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
-            correctAnswer: "Carbon Dioxide"
-        },
-        {
-            question: "What is the largest organ in the human body?",
-            options: ["Heart", "Liver", "Skin", "Lungs"],
-            correctAnswer: "Skin"
-        },
-        {
-            question: "Which country is famous for the ancient pyramids?",
-            options: ["Greece", "Egypt", "Italy", "India"],
-            correctAnswer: "Egypt"
-        },
-        {
-            question: "What is the chemical symbol for water?",
-            options: ["Wa", "H2O", "Wo", "Hy"],
-            correctAnswer: "H2O"
-        },
-        {
-            question: "How many continents are there on Earth?",
-            options: ["3", "5", "6", "7"],
-            correctAnswer: "7"
-        },
-        {
-            question: "What is the smallest prime number?",
-            options: ["0", "1", "2", "3"],
-            correctAnswer: "2"
-        }
+        // Add more questions here
     ];
 
     let currentQuestion = 0;
@@ -57,38 +18,48 @@ $(document).ready(function () {
 
     function loadQuestion() {
         const questionData = questions[currentQuestion];
-        $(".question").text(`Question ${currentQuestion + 1}: ${questionData.question}`);
+        $(".question").text(questionData.question);
         $(".options .option").each(function (index) {
             $(this).text(questionData.options[index]);
             $(this).css("background-color", "");
+            $(this).off("click");
+            $(this).on("click", function () {
+                if (answered) return;
+
+                answered = true;
+                $(this).css("background-color", "#85f00e"); // Correct color
+
+                const selectedOption = $(this).text();
+                const correctAnswer = questions[currentQuestion].correctAnswer;
+
+                if (selectedOption === correctAnswer) {
+                    $(".feedback").text("Correct! " + correctAnswer + " is the correct answer.");
+                } else {
+                    $(".feedback").text("Incorrect. The correct answer is " + correctAnswer + ".");
+                    $(".option:contains('" + correctAnswer + "')").css("background-color", "#f00505"); // Highlight correct answer
+                }
+
+                $("#next-button").show();
+            });
         });
-        $(".feedback").text("");
+
         answered = false;
     }
 
+    loadQuestion();
+
     $("#start-button").on("click", function () {
+        currentQuestion = 0;
         loadQuestion();
         $("#start-button").hide();
-        $("#next-button").show();
+        $("#next-button").hide();
     });
 
     $("#next-button").on("click", function () {
         if (currentQuestion < questions.length - 1) {
             currentQuestion++;
             loadQuestion();
-            $(".option").on("click", function () {
-                if (answered) return;
-                answered = true;
-                $(this).css("background-color", "#85f00e");
-                const selectedOption = $(this).text();
-                const correctAnswer = questions[currentQuestion].correctAnswer;
-                if (selectedOption === correctAnswer) {
-                    $(".feedback").text("Correct! " + correctAnswer + " is the correct answer.");
-                } else {
-                    $(".feedback").text("Incorrect. The correct answer is " + correctAnswer + ".");
-                    $(".option:contains('" + correctAnswer + "')").css("background-color", "#85f00e");
-                }
-            });
+            $("#next-button").hide();
         } else {
             $(".feedback").text("Quiz completed.");
             $("#next-button").hide();
