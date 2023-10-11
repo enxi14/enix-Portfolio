8e6ea385b54f3e092071eb8059d9316fb1398e99
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-10-11 05:16:17 -0400

    commit

--- project-Enix/javascript/quiz.js
+++ project-Enix/javascript/quiz.js
@@ -1,17 +1,57 @@
 $(document).ready(function () {
-    const questions = [
-        {
-            question: "What is the capital of France?",
-            options: ["Paris", "London", "Berlin", "Madrid"],
-            correctAnswer: "Paris"
-        },
-        {
-            question: "Which planet is known as the Red Planet?",
-            options: ["Mars", "Venus", "Jupiter", "Mercury"],
-            correctAnswer: "Mars"
-        },
-        // Add more questions here
-    ];
+const questions = [
+    {
+        question: "What anime features a ninja named Naruto Uzumaki?",
+        options: ["Bleach", "One Piece", "Naruto", "Dragon Ball"],
+        correctAnswer: "Naruto"
+    },
+    {
+        question: "In which anime series does a group of kids enter the Digital World?",
+        options: ["Pokémon", "Yu-Gi-Oh!", "Digimon", "Sailor Moon"],
+        correctAnswer: "Digimon"
+    },
+    {
+        question: "What is the name of the main character in 'Attack on Titan'?",
+        options: ["Eren Yeager", "Goku", "Monkey D. Luffy", "Ichigo Kurosaki"],
+        correctAnswer: "Eren Yeager"
+    },
+    {
+        question: "Which anime is known for its 'Sharingan' and 'Rasengan' techniques?",
+        options: ["One Punch Man", "Naruto", "Attack on Titan", "Dragon Ball Z"],
+        correctAnswer: "Naruto"
+    },
+    {
+        question: "What popular anime is set in the 'Soul Society' and features Shinigami (soul reapers)?",
+        options: ["Naruto", "Bleach", "One Piece", "Death Note"],
+        correctAnswer: "Bleach"
+    },
+    {
+        question: "In which American animated series do the characters live in a fictional underwater city called Bikini Bottom?",
+        options: ["The Simpsons", "South Park", "SpongeBob SquarePants", "Family Guy"],
+        correctAnswer: "SpongeBob SquarePants"
+    },
+    {
+        question: "What animated series follows the adventures of a boy and his magical dog in a post-apocalyptic world?",
+        options: ["Adventure Time", "Regular Show", "Futurama", "American Dad!"],
+        correctAnswer: "Adventure Time"
+    },
+    {
+        question: "What is the name of the animated series in which a boy discovers a portal to other dimensions and has a portal gun?",
+        options: ["The Fairly OddParents", "Rick and Morty", "The Powerpuff Girls", "Dexter's Laboratory"],
+        correctAnswer: "Rick and Morty"
+    },
+    {
+        question: "In which animated show do four boys often get into bizarre and surreal adventures?",
+        options: ["Rick and Morty", "Futurama", "South Park", "The Simpsons"],
+        correctAnswer: "South Park"
+    },
+    {
+        question: "Which animated series features the Griffins, a dysfunctional family living in the fictional town of Quahog, Rhode Island?",
+        options: ["The Simpsons", "American Dad!", "South Park", "Family Guy"],
+        correctAnswer: "Family Guy"
+    }
+];
+
 
     let currentQuestion = 0;
     let answered = false;
@@ -27,7 +67,7 @@ $(document).ready(function () {
                 if (answered) return;
 
                 answered = true;
-                $(this).css("background-color", "#85f00e"); // Correct color
+                $(this).css("background-color", "#0884c7"); // Correct color
 
                 const selectedOption = $(this).text();
                 const correctAnswer = questions[currentQuestion].correctAnswer;
