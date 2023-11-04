97ab1c26aa2d624c461f8608669bfc6c7f668786
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-11-03 21:42:27 -0400

    commit

--- project-Enix/javascript/contact.js
+++ project-Enix/javascript/contact.js
@@ -1,12 +1,18 @@
 function collectFormData() {
-        var name = document.getElementById("name").value;
-        var email = document.getElementById("email").value;
+            var name = document.getElementById("name").value;
+            var email = document.getElementById("email").value;
+            var age = document.getElementById("age").value;
+            var country = document.getElementById("country").value;
+            var interests = document.getElementById("interests").value;
 
-        var formData = {
-            "name": name,
-            "email": email
-        };
+            var formData = {
+                "name": name,
+                "email": email,
+                "age": age,
+                "country": country,
+                "interests": interests
+            };
 
-        // Display JSON data
-        document.getElementById("jsonDataDisplay").innerText = JSON.stringify(formData, null, 2);
-    }
+            // Display JSON data
+            document.getElementById("jsonDataDisplay").innerHTML = "<pre>" + JSON.stringify(formData, null, 2) + "</pre>";
+        }
