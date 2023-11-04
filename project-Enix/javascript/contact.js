5254d8c482b705dc457d5f64b1315aceab70d2e7
enxi14 <36089987+enxi14@users.noreply.github.com>
2023-11-03 22:14:40 -0400

    commit

--- project-Enix/javascript/contact.js
+++ project-Enix/javascript/contact.js
@@ -21,4 +21,25 @@ async function collectFormData() {
             setTimeout(() => {
                 jsonDataDisplay.innerHTML = "<pre>" + JSON.stringify(formData, null, 2) + "</pre>";
             }, 1500);
-        }
+}
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', async (req, res) => {
+    try {
+        // Simulate an asynchronous operation, e.g., fetching data from a database
+        await new Promise(resolve => setTimeout(resolve, 2000));
+
+        res.send('Hello, this is your asynchronous server!');
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
+});
+
